9977f59bf0285bcb0019020b50da1b82ec90254e
rubyrubysnell <ruby@rubysnell.co.uk>
2024-01-30 14:29:25 +0000

    change for loop to while loop

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-    
+
     // red, blue, yellow, white, black
     const possibleColors = ["#f50f0f", "#0d7fbe", "#fae317", "#f3f3f3", "#000000"];
 
@@ -11,16 +11,23 @@ function setup() {
 
     // canvas size in pixels
     const canvasSize = squareSize * cellNumber
-    
+
+    // set width and height
+    const width = squareSize
+    const height = squareSize
+
     noStroke(0);
     createCanvas(canvasSize, canvasSize);
     background(220);
 
-    for(let row = 0; row < cellNumber; row++){
-        for(let column = 0; column < cellNumber; column++){
-            const red = random(255);
-            const green = random(255);
-            const blue = random(255);
+    let x = 0
+    let y = 0
+
+    while (y < canvasSize) {
+
+        x = 0
+
+        while (x < canvasSize) {
 
             // choose random hex code from array
             const colorCode = random(possibleColors)
@@ -30,21 +37,23 @@ function setup() {
             // fill all the square with one colour
             fill(myColor)
 
-            // calculate horizontal and vertical positions in pixels
-            const x = (row * squareSize) 
-            const y = (column * squareSize) 
-
-            // set width and height
-            const width = squareSize 
-            const height = squareSize 
-
-            // square(x, y, s)
+            // rect(x, y, width, height)
             // x = position of the left of the square
             // y = position of the top of the square
             // s = size of the side of the square
             // width = width in pixels of the rectangle
             // height = height in pixels of the rectangle
             rect(x, y, width, height)
+
+            // update x
+
+            x = x + width
         }
+
+        // update y 
+
+        y = y + width
+
     }
 }
+
